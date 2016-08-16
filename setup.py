bb4ce8d3784a588dcc602cec2ce3b5d362499712
lamter <lamter.fu@gmail.com>
2016-08-16 22:28:30 +0800

    - add : 确定框架

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
+__version__ = "0.1.0"
+
+setup(name='slaveo',
+      version=__version__,
+      keywords='slaveo',
+      description='策略库',
+      long_description=read("README.md"),
+
+      url='https://github.com/lamter/slaveo',
+      author='lamter',
+      author_email='lamter.fu@gmail.com',
+
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=read("requirements.txt").splitlines(),
+      classifiers=['Development Status :: 4 - Beta',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   'Programming Language :: Python :: 3.5',
+                   'License :: OSI Approved :: MIT License'],
+      )
